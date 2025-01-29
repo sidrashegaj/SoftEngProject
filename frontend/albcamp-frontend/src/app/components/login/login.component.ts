aef825b2b31b02b1def052e03e0cc84b3cdfbc1d
sidrashegaj <158463285+sidrashegaj@users.noreply.github.com>
2025-01-29 21:33:45 +0100

    login and regsiter view

---
+++ frontend/albcamp-frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+// import { FlashMessageService } from '../../services/flash-message.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  imports: [FormsModule, CommonModule]
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+  error = '';
+
+  constructor(private authService: AuthService, private router: Router, 
+    // private flashMessageService: FlashMessageService
+    ) {}
+
+  
+  onSubmit() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (res) => {
+        this.router.navigate(['/campgrounds']); 
+      },
+      error: (err) => {
+        console.error('Login error', err);
+        // this.flashMessageService.showMessage('Login failed!', 5000);
+      }
+    });
+  }
+  
+}
